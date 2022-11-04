f6d6c41e380cb1dea1556b96f51d7c3cc2fb59db
June Rhomel Mandigma <104764941+JuneRhomel@users.noreply.github.com>
2022-11-04 11:36:46 +0800

    home

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let chat = document.querySelector("#chat");
+
+function exit(){
+    document.getElementById("chat").style.display = "none";
+}
+
+function chatBtn(){
+    document.getElementById("chat").style.display = "block";
+}
