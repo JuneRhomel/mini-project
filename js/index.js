ed4d654afb02b3e34d24a922e817a97634eab152
June Rhomel Mandigma <104764941+JuneRhomel@users.noreply.github.com>
2022-11-08 09:38:16 +0800

    final

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // chat
 
-function exit(){
+function exit() {
     document.getElementById("chat").style.display = "none";
 }
 
-function chatBtn(){
+function chatBtn() {
     document.getElementById("chat").style.display = "block";
 }
 
@@ -21,24 +21,24 @@ const submit = document.querySelector("#submit-login");
 
 const errorForm = document.querySelector("#form")
 
-errorForm.addEventListener ("submit", function(e){
-    if (email === "" || email === null) {  
+errorForm.addEventListener("submit", function (e) {
+    if (email === "" || email === null) {
         emailError.innerText = "Email is Required";
         e.preventDefault();
     }
-    if (  password.value.length <= 6  ) {
-        passwordlError.innerHTML =  "Password must be longer than 6 characters";
+    if (password.value.length <= 6) {
+        passwordlError.innerHTML = "Password must be longer than 6 characters";
         e.preventDefault();
     }
-    if (password.value.length >= 20 ) {
+    if (password.value.length >= 20) {
         passwordlError.innerHTML = "Your password is too long";
         e.preventDefault();
     }
-    if (password.value === "1234567" ) {
+    if (password.value === "1234567") {
         passwordlError.innerHTML = "Password 1234567 is not accepted";
         e.preventDefault();
     }
-    
+
 });
 
 
@@ -59,7 +59,7 @@ const countdown = () => {
     const now = new Date().getTime();
     const gap = countDate - now;
     // Time works 
-    const second  = 1000;
+    const second = 1000;
     const minuts = second * 60;
     const hour = minuts * 60;
     const day = hour * 24;
@@ -72,10 +72,32 @@ const countdown = () => {
 
 
     // Display in DOM 
-    document.querySelector ("#day").innerHTML = textDay;
-    document.querySelector ("#hour").innerHTML = textHour;
-    document.querySelector ("#minute").innerHTML = textMinuts;
-    document.querySelector ("#second").innerHTML = textSecond;
+    document.querySelector("#day").innerHTML = textDay;
+    document.querySelector("#hour").innerHTML = textHour;
+    document.querySelector("#minute").innerHTML = textMinuts;
+    document.querySelector("#second").innerHTML = textSecond;
 };
 
 setInterval(countdown, 1000)
+
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
